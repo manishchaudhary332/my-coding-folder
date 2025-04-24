e5f88b601ef334471773a53adaf06b23212aab37
Manish Chaudhary <manishchaudhary3803@gmail.com>
2025-04-24 12:09:31 +0530

    LAST logic OF USE EFFEC

---
+++ UseEffect/public/Useeffect4.jsx
@@ -0,0 +1,19 @@
+import React, { useEffect, useState } from 'react';
+
+function Useeffect4() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(res => res.json())
+      .then(data => setUsers(data));
+  }, []);
+
+  return (
+    <ul>
+      {users.map(user => <li key={user.id}>{user.name}</li>)}
+    </ul>
+  );
+}
+
+export default Useeffect4
--- UseEffect/src/main.jsx
+++ UseEffect/src/main.jsx
@@ -4,11 +4,13 @@ import './index.css'
 import App from './App.jsx'
 import Useeffect2 from './Useeffect2.jsx'
 import Useeffect3 from './Useeffect3.jsx'
+import Useeffect4 from '../public/Useeffect4.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
     <Useeffect2/>
     <Useeffect3/>
+    <Useeffect4/>
   </StrictMode>,
 )
